cb12855baefdf3c5ef964bf020f08094747c0c82
Carlos Roman <carlos@cliche-corp.co.uk>
2019-01-07 04:43:41 +0000

    :construction: Noop cmd for emulator

---
+++ cmd/noop/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"github.com/carlosroman/go-chip-8/internal/pkg/cmd"
+	"github.com/carlosroman/go-chip-8/internal/pkg/noop"
+	"github.com/carlosroman/go-chip-8/pkg/cpu"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.SetLevel(log.InfoLevel)
+	s := &noop.Screen{}
+	ctx, _ := context.WithCancel(context.Background())
+	c := cmd.GetCommand(ctx, s, cpu.NewKeyboard(), s)
+	if err := c.Execute(); err != nil {
+		log.WithError(err).Fatal("app crashed")
+	}
+}
---
+++ internal/pkg/noop/screen.go
@@ -0,0 +1,36 @@
+package noop
+
+import (
+	"context"
+	"github.com/carlosroman/go-chip-8/pkg/cpu"
+	log "github.com/sirupsen/logrus"
+	"sync"
+	"time"
+)
+
+type Screen struct {
+	lock sync.Mutex
+	fb   []byte
+}
+
+func (s *Screen) Draw(frameBuffer []byte) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	log.Info("Draw")
+	copy(s.fb, frameBuffer)
+}
+
+func (s *Screen) Refresh() error {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	log.Info("Refresh")
+	return nil
+}
+
+func (n *Screen) Run(ctx context.Context) error {
+	go func() {
+		cpu.Start("loop", ctx, time.Second, n.Refresh)
+	}()
+	<-ctx.Done()
+	return nil
+}
